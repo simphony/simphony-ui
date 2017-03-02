f1f2669dba3061937e13ce4eb5bb10464222ed1f
martinRenou <martin.renou@isae.fr>
2017-03-02 22:36:44 +0000

    Add a first dummy test

---
+++ simphony_ui/tests/test.py
@@ -0,0 +1,19 @@
+"""
+Tests
+"""
+
+import unittest
+
+
+class DummyTest(unittest.TestCase):
+
+    def setUp(self):
+        """ Test initialization """
+        self.a = 3
+
+    def test_a(self):
+        self.assertEqual(self.a, 3)
+        self.assertNotEqual(self.a, 56)
+
+if __name__ == '__main__':
+    unittest.main()
