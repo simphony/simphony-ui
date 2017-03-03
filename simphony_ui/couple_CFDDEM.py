91c143a2e3d18f0814ab6201500879e5413a91aa
martinRenou <martin.renou@isae.fr>
2017-03-03 17:13:44 +0000

    Fix path of the DEM input file

--- simphony_ui/couple_CFDDEM.py
+++ simphony_ui/couple_CFDDEM.py
@@ -85,7 +85,7 @@ timestep_DEM = 1e-6
 # number of bins for velo profile generation
 Nbins_lmp = Nbins_OF
 
-restart_file = os.path.abspath(os.path.join("simphony_ui", "DEM_input.dat"))
+restart_file = os.path.join(os.path.dirname(__file__), "DEM_input.dat")
 
 
 if mode_OF != "none":
