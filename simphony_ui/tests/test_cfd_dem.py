de12f418e572a6da133e0f255c6f24cae0d96c3c
martinRenou <martin.renou@isae.fr>
2017-03-06 15:10:23 +0000

    The test now safely creates a temporary directory where to put the output files

--- simphony_ui/tests/test_cfd_dem.py
+++ simphony_ui/tests/test_cfd_dem.py
@@ -6,16 +6,20 @@ import unittest
 import simphony_ui.couple_CFDDEM as cfd_dem
 import shutil
 import os
+import tempfile
 
 
 class TestCfeDem(unittest.TestCase):
 
     def setUp(self):
-        self.output_dir = 'test_mesh'
+        self.tmp_dir = tempfile.mkdtemp()
+        self.mesh_name = 'test_mesh'
 
     def test_output(self):
-        cfd_dem.main()
-        self.assertTrue(os.path.exists(self.output_dir))
+        cfd_dem.main(self.tmp_dir, self.mesh_name)
+        self.assertTrue(os.path.exists(
+            os.path.join(self.tmp_dir, self.mesh_name)
+        ))
 
     def tearDown(self):
-        shutil.rmtree(self.output_dir, ignore_errors=True)
+        shutil.rmtree(self.tmp_dir)
