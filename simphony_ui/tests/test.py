14c183863f559f835a62d091d92eb3d60ecc08da
martinRenou <martin.renou@isae.fr>
2017-03-06 14:21:12 +0000

    Remove redundant test

--- simphony_ui/tests/test.py
+++ simphony_ui/tests/test.py
@@ -13,15 +13,6 @@ output_dir = 'test_mesh'
 
 class CfeDem(unittest.TestCase):
 
-    def test_run(self):
-        try:
-            cfd_dem.main()
-        except:
-            self.fail('Error when running the main script')
-        finally:
-            # remove output if it exists
-            shutil.rmtree(output_dir, ignore_errors=True)
-
     def test_output(self):
         cfd_dem.main()
         self.assertTrue(os.path.exists(output_dir))
