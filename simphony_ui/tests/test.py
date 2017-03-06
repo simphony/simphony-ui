153e09b4032c6baad597ecefca8ef7467549c819
martinRenou <martin.renou@isae.fr>
2017-03-06 13:08:35 +0000

    The test now remove output files after running the main script

--- simphony_ui/tests/test.py
+++ simphony_ui/tests/test.py
@@ -4,6 +4,10 @@ Tests
 
 import unittest
 import simphony_ui.couple_CFDDEM as cfd_dem
+import shutil
+
+
+output_dir = 'test_mesh'
 
 
 class CFD_DEM(unittest.TestCase):
@@ -13,3 +17,6 @@ class CFD_DEM(unittest.TestCase):
             cfd_dem.main()
         except:
             self.fail('Error when running the main script')
+        finally:
+            # remove output if it exists
+            shutil.rmtree(output_dir, ignore_errors=True)
