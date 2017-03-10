39c345a0ae030f4ced0dc3a4163e5f76c6d7f211
martinRenou <martin.renou@isae.fr>
2017-03-10 10:57:43 +0000

    Add openfoam wrapper settings

--- simphony_ui/openfoam_wrapper_creation.py
+++ simphony_ui/openfoam_wrapper_creation.py
@@ -13,4 +13,21 @@ def create_openfoam_wrapper(openfoam_settings):
         openfoam_wrapper = openfoam_file_io.Wrapper()
         openfoam_cuba_ext = openfoam_file_io.CUBAExt
 
+    # Set Computational method parameters
+    openfoam_wrapper.CM_extensions[openfoam_cuba_ext.GE] = \
+        (openfoam_cuba_ext.INCOMPRESSIBLE, openfoam_cuba_ext.LAMINAR_MODEL)
+
+    openfoam_wrapper.CM[CUBA.NAME] = openfoam_settings.mesh_name
+
+    # Set system parameters/ conditions
+    openfoam_wrapper.SP[CUBA.TIME_STEP] = openfoam_settings.timestep
+    openfoam_wrapper.SP[CUBA.NUMBER_OF_TIME_STEPS] = \
+        openfoam_settings.num_iterations
+    openfoam_wrapper.SP[CUBA.DENSITY] = openfoam_settings.density
+    openfoam_wrapper.SP[CUBA.DYNAMIC_VISCOSITY] = openfoam_settings.viscosity
+
+    # Set boundary conditions
+    openfoam_wrapper.BC[CUBA.VELOCITY] = {}
+    openfoam_wrapper.BC[CUBA.PRESSURE] = {}
+
     return openfoam_wrapper
