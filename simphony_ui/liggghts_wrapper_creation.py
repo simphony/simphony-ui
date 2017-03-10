2d2f9fbf36743543a96d8d1d8e3839f25bc02462
martinRenou <martin.renou@isae.fr>
2017-03-10 16:58:46 +0000

    Add documentation

--- simphony_ui/liggghts_wrapper_creation.py
+++ simphony_ui/liggghts_wrapper_creation.py
@@ -3,6 +3,19 @@ from simphony.core.cuba import CUBA
 
 
 def create_liggghts_wrapper(liggghts_settings):
+    """ Creates the liggghts wrapper setup from the settings as provided
+    by the model object
+
+    Parameters
+    ----------
+    liggghts_settings : LiggghtsModel
+        The traited model describing the liggghts parameters
+
+    Returns
+    -------
+    liggghts_wrapper : LiggghtsWrapper
+        an instance of a LiggghtsWrapper, properly configured.
+    """
     liggghts_wrapper = liggghts.LiggghtsWrapper(use_internal_interface=True)
 
     # Set Computational method parameters
