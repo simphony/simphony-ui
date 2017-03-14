b31426dd00259ab2ccb7c953fa57d6828b872f8d
martinRenou <martin.renou@isae.fr>
2017-03-14 16:57:47 +0000

    Add tests for the force computation

--- simphony_ui/tests/test_run_calculation.py
+++ simphony_ui/tests/test_run_calculation.py
@@ -9,6 +9,7 @@ from simphony.core.cuds_item import CUDSItem
 from simphony.core.cuba import CUBA
 from simphony_ui.ui import Application
 from simphony_ui.tests.test_utils import cleanup_garbage
+from simphony_ui.couple_openfoam_liggghts import compute_drag_force
 
 
 class TestCalculation(unittest.TestCase):
@@ -65,3 +66,78 @@ class TestCalculation(unittest.TestCase):
             avg_velo += cell.data[CUBA.VELOCITY][0]
         avg_velo = avg_velo/mesh_dataset.count_of(CUDSItem.CELL)
         self.assertAlmostEqual(avg_velo, 0.00151286)
+
+
+class TestForceComputation(unittest.TestCase):
+
+    def setUp(self):
+        self.radius = 0.02
+        self.rel_velo = [1.2, 5.2, 0.2]
+        self.viscosity = 0.0062
+        self.density = 1005.2
+        self.force_type = None
+
+    def test_stokes(self):
+        self.force_type = 'Stokes'
+        force = compute_drag_force(
+            self.force_type,
+            self.radius,
+            self.rel_velo,
+            self.viscosity,
+            self.density
+        )
+        self.assertListEqual(
+            force.tolist(),
+            [
+                0.0028048139211249673,
+                0.012154193658208192,
+                0.00046746898685416123
+            ]
+        )
+
+    def test_dala(self):
+        self.force_type = 'Dala'
+        force = compute_drag_force(
+            self.force_type,
+            self.radius,
+            self.rel_velo,
+            self.viscosity,
+            self.density
+        )
+        self.assertListEqual(
+            force.tolist(),
+            [
+                1.7406892534402443,
+                7.542986764907726,
+                0.29011487557337406
+            ]
+        )
+
+    def test_coul(self):
+        self.force_type = 'Coul'
+        force = compute_drag_force(
+            self.force_type,
+            self.radius,
+            self.rel_velo,
+            self.viscosity,
+            self.density
+        )
+        self.assertListEqual(
+            force.tolist(),
+            [
+                1.3010027654950582,
+                1.3010027654950582,
+                1.3010027654950582
+            ]
+        )
+
+    def test_unknown_force(self):
+        self.force_type = 'Coucou heho'
+        with self.assertRaises(ValueError):
+            compute_drag_force(
+                self.force_type,
+                self.radius,
+                self.rel_velo,
+                self.viscosity,
+                self.density
+            )
