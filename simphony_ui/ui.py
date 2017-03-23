2271af1c78e120b2562736c3bee3cbcb97521d91
martinRenou <martin.renou@isae.fr>
2017-03-23 11:01:07 +0000

    Reviews

--- simphony_ui/ui.py
+++ simphony_ui/ui.py
@@ -264,7 +264,10 @@ class Application(HasStrictTraits):
         GUI.invoke_later(self.progress_dialog.update, progress)
 
     def _logger_default(self):
-        return logging.getLogger(self.__class__.__name__)
+        return logging.getLogger('{}.{}'.format(
+            self.__class__.__module__,
+            self.__class__.__name__)
+        )
 
     def __executor_default(self):
         return futures.ThreadPoolExecutor(max_workers=1)
